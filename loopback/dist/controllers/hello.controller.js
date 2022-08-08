332c2d8ea610dfad902dd0a3c14fa650c434746e
Ankit Jaiswal <ankitjaiswaall@gmail.com>
2022-08-08 19:10:56 +0530

    Add files via upload

---
+++ loopback/dist/controllers/hello.controller.js
@@ -0,0 +1,20 @@
+"use strict";
+// Uncomment these imports to begin using these cool features!
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HelloController = void 0;
+const tslib_1 = require("tslib");
+const rest_1 = require("@loopback/rest");
+// import {inject} from '@loopback/core';
+class HelloController {
+    hello() {
+        return ("hello from hello controller");
+    }
+}
+tslib_1.__decorate([
+    (0, rest_1.get)('/hello'),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", []),
+    tslib_1.__metadata("design:returntype", String)
+], HelloController.prototype, "hello", null);
+exports.HelloController = HelloController;
+//# sourceMappingURL=hello.controller.js.map
